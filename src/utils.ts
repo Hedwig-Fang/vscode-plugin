4ec4b8f00b0a22669242ea28e90b47d46ff761e4
喵喵君 <706964007@qq.com>
2024-01-05 00:27:15 +0800

    feat: monorepo支持父级目录的配置在子级目录也生效

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,52 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-export function getFullFilePath(relativePath: string): string | undefined {
-  // 获取当前打开的工作区文件夹
-  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+import * as fs from 'fs';
 
-  if (workspaceFolder) {
+
+function hasSeetingsFile(folderPath: string) {
+  // 检查文件夹中是否存在 settings.json 文件
+  const settingsFilePath = path.join(folderPath as unknown as string,'.vscode', 'settings.json');
+  return fs.existsSync(settingsFilePath);
+}
+export function findSettingsFile(folderPath: string) {
+  if(hasSeetingsFile(folderPath)) {
+    return {
+      folder: folderPath,
+      url: path.join(folderPath as unknown as string,'.vscode', 'settings.json')
+    };
+  }
+  let currentPath = folderPath;
+  while (currentPath) {
+    const settingsFilePath = path.join(currentPath, '.vscode', 'settings.json');
+    if (fs.existsSync(settingsFilePath)) {
+        return  {
+          foler: currentPath,
+          url:settingsFilePath
+        };
+    }
+
+    const parentPath = path.dirname(currentPath);
+    if (parentPath === currentPath) {
+        // Reached the root directory
+        break;
+    }
+
+    currentPath = parentPath;
+}
+return {
+  foler: '',
+  url: ''
+}
+
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getFullFilePath(relativePath: string, fsPath: any): string | undefined {
+  // 获取当前打开的工作区文件夹
+// const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+
+  if (fsPath) {
       // 使用 path.join() 将相对路径连接到工作区文件夹路径
-      const fullPath = path.join(workspaceFolder.uri.fsPath, relativePath);
+      const fullPath = path.join(fsPath, relativePath);
       return fullPath;
   }
 
